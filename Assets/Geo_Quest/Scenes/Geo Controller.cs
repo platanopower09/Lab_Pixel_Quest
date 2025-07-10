55237eba044bd645666d8ca97388e638a1fec2e9
platanopower09 <jrdp103@gmail.com>
2025-07-10 12:12:46 -0400

    yaroa

--- Assets/Geo_Quest/Scenes/Geo Controller.cs
+++ Assets/Geo_Quest/Scenes/Geo Controller.cs
@@ -1,27 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GeoController : MonoBehaviour
 {
-    private int varOne = 1;
-    int var3= 3;
-    private string bacon = "salami";
-    private int v;
-
+    private Rigidbody2D rb;
+   int varTwo = 3; 
+    public int speed = 3;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         int var2 = 2;
         Debug.Log("Hello World");
         string milk = "what?";
-        Debug.Log(bacon+milk);
+        
+     
     }
 
     // Update is called once per frame
     void Update()
+
+
     {
-        Debug.Log(var3++);
-        var3++;
+        float xInput = Input.GetAxis("Horizontal");
+        Debug.Log(xInput);
+        rb.velocity = new Vector2(xInput * speed,  rb.velocity.y); 
+
+        /*
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S)) { 
+        transform.position += new Vector3(0, -1, 0); }
+        
+    }*/
+        OnCollisionEnter2D(Collision2D collision) 
+    }
+}
+
+internal class rb
+{
+    private int v;
+    private Vector2 velocity;
+
+    public rb(int v, Vector2 velocity)
+    {
+        this.v = v;
+        this.velocity = velocity;
     }
 }
