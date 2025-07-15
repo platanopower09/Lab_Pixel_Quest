7a822ca59ff98f1f9f4566d98b3db5355279f81a
platanopower09 <jrdp103@gmail.com>
2025-07-15 15:39:40 -0400

    proyecto

--- Assets/Geo_Quest/Scenes/Geo Controller.cs
+++ Assets/Geo_Quest/Scenes/Geo Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 using System.Transactions;
+using Unity.VisualScripting;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,31 +12,22 @@ using UnityEngine.UIElements;
 public class GeoController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer rbSprite;
    int varTwo = 3;
     public int fast = 7;
     private object groundCheck;
     private object isGrounded;
     private object position;
     private readonly object groundCheckRadius;
+    public string NextLevel = "Scene_2";
 
-    public object Physics2S { get; private set; }
-
-    public class PlayerJump : MonoBehaviour
-    {
-        public float jumpForce = 7f;
-        public LayerMask groundLayer;
-        public Transform groundCheck;
-        public float groundCheckRadius = 0.2f;
-        private Rigidbody2D rb;
-        private bool isGrounded; 
-
-
-    }
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         int var2 = 2;
+        rbSprite = GetComponent<SpriteRenderer>();
+
         
 
         
@@ -45,32 +37,25 @@ public class GeoController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-
-
     {
         float xInput = Input.GetAxis("Horizontal");
        //ebug.Log(xInput);
         rb.velocity = new Vector2(xInput * fast,  rb.velocity.y);
-
-        /*
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            transform.position += new Vector3(0, 1, 0);
+            rbSprite.color = Color.grey;
         }
-
-        if (Input.GetKeyDown(KeyCode.A)) {
-            transform.position += new Vector3(-1, 0, 0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            transform.position += new Vector3(1, 0, 0);
+            rbSprite.color = Color.green; 
         }
-        if (Input.GetKeyDown(KeyCode.S)) { 
-        transform.position += new Vector3(0, -1, 0); }
-        
-    }*/
-    }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            rbSprite.color = Color.yellow; 
+        }
+
+    } 
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.tag)
@@ -80,6 +65,12 @@ public class GeoController : MonoBehaviour
                     string thisLevel = SceneManager.GetActiveScene().name;
                     SceneManager.LoadScene(thisLevel); 
                     break; 
+                    
+                }
+            case "Next Level":
+                {
+                    SceneManager.LoadScene(NextLevel);
+                    break;
                 }
         }
 
