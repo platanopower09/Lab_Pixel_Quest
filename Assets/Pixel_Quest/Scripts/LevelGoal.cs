cd680b87df3fb03c6b4537997b913cae16aed156
platanopower09 <jrdp103@gmail.com>
2025-07-23 14:39:21 -0400

    molondrones

--- Assets/Pixel_Quest/Scripts/LevelGoal.cs
+++ Assets/Pixel_Quest/Scripts/LevelGoal.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LevelGoal : MonoBehaviour
 {
+    public string nextLevel = "Level_2";
     // Start is called before the first frame update
     void Start()
     {
